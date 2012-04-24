e2a30cc777333a20374c2777ac2c80e94953bb51
Jie Tang <jietang86@gmail.com>
2012-04-24 05:04:35 -0700

    recentf, savehist

--- config/02-jt-backups.el
+++ config/02-jt-backups.el
@@ -4,3 +4,17 @@
 ;; (setq version-control t)
 ;; (push (cons "." emacs-backups-dir)
 ;;       backup-directory-alist)
+(setq recentf-save-file (concat emacs-backups-dir "recentf")
+      recentf-max-saved-items 200
+      recentf-max-menu-items 15)
+(recentf-mode t)
+
+;; savehist keeps track of some history
+(setq savehist-additional-variables
+      ;; search entries
+      '(search ring regexp-search-ring)
+      ;; save every minute
+      savehist-autosave-interval 60
+      ;; keep the home clean
+      savehist-file (concat user-emacs-directory "savehist"))
+(savehist-mode t)
