668a4910fdfa2fcf3302f474dcd48bca7328529e
Jie Tang <jietang86@gmail.com>
2012-04-24 04:33:52 -0700

    disable backups

--- config/02-jt-backups.el
+++ config/02-jt-backups.el
@@ -1,6 +1,6 @@
 ;; Backups
 ;; set a directory for backup files instead of dumping them all over the system
 ; (setq make-backup-files t) ; the default
-(setq version-control t)
-(push (cons "." emacs-backups-dir)
-      backup-directory-alist)
+;; (setq version-control t)
+;; (push (cons "." emacs-backups-dir)
+;;       backup-directory-alist)
