43167a98abaddc64e7b9ce3856c978b1ffd24067
Jie Tang <jie@dbdev5.(none)>
2012-05-03 00:20:49 -0700

    make solarized work on terminal...

--- config/01-jt-color-theme.el
+++ config/01-jt-color-theme.el
@@ -1,4 +1,4 @@
 (add-to-list 'custom-theme-load-path (concat thirdparty-elisp-dir "emacs-color-theme-solarized/"))
 
-(setq solarized-contrast 'high)
+(setq solarized-contrast 'low)
 (load-theme 'solarized-dark t)
