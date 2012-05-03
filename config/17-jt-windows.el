b64427407c01946400362d40f4ebf50ed3d51804
Jie Tang <jie@dbdev5.(none)>
2012-05-03 00:21:30 -0700

    fix windmove on terminal

--- config/17-jt-windows.el
+++ config/17-jt-windows.el
@@ -1,5 +1,8 @@
 ;; Shift+direction
-(windmove-default-keybindings)
+(global-set-key (kbd "M-j") 'windmove-left)
+(global-set-key (kbd "M-l") 'windmove-right)
+(global-set-key (kbd "M-i") 'windmove-up)
+(global-set-key (kbd "M-k") 'windmove-down)
 
 ;; C-x o moves one direction, C-x O moves the other
 (global-set-key (kbd "C-x O") (lambda () (interactive) (other-window -1)))
