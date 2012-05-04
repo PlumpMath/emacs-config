b9325516b03a598ab781d68f6118d4e60975363b
Jie Tang <jie@dbdev5.(none)>
2012-05-03 17:34:51 -0700

    revert bad windmove bindings

--- config/17-jt-windows.el
+++ config/17-jt-windows.el
@@ -1,8 +1,5 @@
 ;; Shift+direction
-(global-set-key (kbd "M-j") 'windmove-left)
-(global-set-key (kbd "M-l") 'windmove-right)
-(global-set-key (kbd "M-i") 'windmove-up)
-(global-set-key (kbd "M-k") 'windmove-down)
+(windmove-default-keybindings)
 
 ;; C-x o moves one direction, C-x O moves the other
 (global-set-key (kbd "C-x O") (lambda () (interactive) (other-window -1)))
