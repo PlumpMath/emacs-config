3dc0cab7f151af6dea410e452d6e51ea6b928b71
name <email>
2015-04-09 22:02:14 -0700

    readd trailing whitespace stripping

--- config/08-jt-saving.el
+++ config/08-jt-saving.el
@@ -1,5 +1,5 @@
 ;; Delete trailing whitespace on save
-;(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; Follow files that are being written to
 (setq-default auto-revert-tail-mode t)
